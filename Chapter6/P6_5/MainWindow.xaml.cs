c3bd53c6285000a634f908c0a7df4228bf6e07af
yf-web <yf_web@163.com>
2020-12-06 21:46:56 +0800

    添加多路绑定

--- Chapter6/P6_5/MainWindow.xaml.cs
+++ Chapter6/P6_5/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,44 @@ namespace P6_5
         public MainWindow()
         {
             InitializeComponent();
+            this.SetMultiBinding();
+        }
+
+        void SetMultiBinding()
+        {
+            var b1 = new Binding("Text") { Source = this.textBox1 };
+            var b2 = new Binding("Text") { Source = this.textBox2 };
+            var b3 = new Binding("Text") { Source = this.textBox3 };
+            var b4 = new Binding("Text") { Source = this.textBox4 };
+
+            var mb = new MultiBinding() { Mode = BindingMode.OneWay };
+            mb.Bindings.Add(b1);
+            mb.Bindings.Add(b2);
+            mb.Bindings.Add(b3);
+            mb.Bindings.Add(b4);
+            mb.Converter = new LogonMultiBindingConverter();
+
+            this.button1.SetBinding(Button.IsEnabledProperty, mb);
+        }
+    }
+
+    public class LogonMultiBindingConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!values.Cast<string>().Any(text=>string.IsNullOrEmpty(text))&&values[0].ToString()==values[1].ToString()&& values[2].ToString() == values[3].ToString())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
