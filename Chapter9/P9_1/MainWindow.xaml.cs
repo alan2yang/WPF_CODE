fe9815c4ad7b7db0f1a0016f0217a5963c5f6d02
yf-web <yf_web@163.com>
2021-03-19 00:12:27 +0800

    添加第九章第一节，关于命令。

---
+++ Chapter9/P9_1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P9_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeCommand();
+        }
+
+        private RoutedCommand clearCommand = new RoutedCommand("Clear",typeof(MainWindow));
+
+        private void InitializeCommand()
+        {
+            this.button1.Command = this.clearCommand;
+            this.clearCommand.InputGestures.Add(new KeyGesture(Key.C,ModifierKeys.Alt));  //设置快捷键
+
+            this.button1.CommandTarget = this.textbox1;
+
+            CommandBinding cb = new CommandBinding();
+            cb.Command = this.clearCommand;
+            cb.CanExecute += new CanExecuteRoutedEventHandler(cb_CanExecute);
+            cb.Executed += new ExecutedRoutedEventHandler(cb_Executed);
+            this.stackPanel.CommandBindings.Add(cb);
+        }
+
+        /// <summary>
+        /// 执行频率比较高
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void cb_CanExecute(object sender,CanExecuteRoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.textbox1.Text))
+            {
+                e.CanExecute = false;
+            }
+            else
+            {
+                e.CanExecute = true;
+            }
+        }
+
+        void cb_Executed(object sender,ExecutedRoutedEventArgs e)
+        {
+            this.textbox1.Clear();
+            e.Handled = true;  //避免继续向上传递
+        }
+
+
+    }
+}
---
+++ Chapter9/P9_2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P9_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.nameTextBox.Text))
+            {
+                e.CanExecute= false;
+            }
+            else
+            {
+                e.CanExecute = true;
+            }
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            string name = this.nameTextBox.Text;
+
+            if (e.Parameter.ToString()=="Teacher")
+            {
+                this.listBoxNewItems.Items.Add($"New Teacher:{name}!");
+            }
+            if (e.Parameter.ToString()=="Student")
+            {
+                this.listBoxNewItems.Items.Add($"New Student:{name}");
+            }
+            e.Handled = true;
+        }
+    }
+}
