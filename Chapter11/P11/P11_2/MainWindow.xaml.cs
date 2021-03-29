2efb3554d095221165979226b0181b57c72663cf
yf-web <yf_web@163.com>
2021-03-29 22:35:43 +0800

    完成DataTemplate

--- Chapter11/P11/P11_2/MainWindow.xaml.cs
+++ Chapter11/P11/P11_2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using P11_2.models;
 
 namespace P11_2
 {
@@ -23,6 +23,36 @@ namespace P11_2
         public MainWindow()
         {
             InitializeComponent();
+            InitiaCarList();
+        }
+
+
+        private void InitiaCarList()
+        {
+            List<Car> carList = new List<Car>()
+            {
+                new Car(){Automaker="1",Name="Diablo",Year="1990",TopSpeed="340"},
+                new Car(){Automaker="2",Name="Murcielago",Year="2001",TopSpeed="353"},
+                new Car(){Automaker="3",Name="Gallaro",Year="2003",TopSpeed="326"},
+                new Car(){Automaker="4",Name="Reventon",Year="2008",TopSpeed="345"},
+                new Car(){Automaker="5",Name="Rev",Year="2018",TopSpeed="450"},
+            };
+            this.listBoxCars.ItemsSource = carList;
+        }
+
+    }
+
+    public class PhotoPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string uriStr = string.Format(@"/Resources/Images/{0}.jpg", (string)value);
+            return new BitmapImage(new Uri(uriStr,UriKind.Relative));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Chapter11/P11/P11_2/Models/Car.cs
+++ Chapter11/P11/P11_2/Models/Car.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace P11_1.models
+namespace P11_2.models
 {
     public class Car
     {
